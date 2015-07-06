13a3d62a86e655d012308c91dec1ccfea2a6aee4
jbocharov <john@voicebase.com>
2015-07-06 16:23:05 -0700

    Update to node v0.12

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+!function(t,a,e,o){"use strict";e.Icon.Default.imagePath="images/";var n=e.map("map",{center:[52.5377,13.3958],zoom:4});new e.tileLayer("http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png",{minZoom:0,maxZoom:18,attribution:'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'}).addTo(n),e.marker([52.5,13.4]).addTo(n)}(window,document,L);
