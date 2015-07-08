6a2e094b890eb101026b9851f4d7bf99b8987f68
jbocharov <john@voicebase.com>
2015-07-07 20:25:57 -0700

    Absorb main.css as visualization.scss, restore header to app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Home from './components/home';
 
 window.React = React;
-const mountNode = document.getElementById('app');
+const mountNode = document.body; //document.getElementById('app');
 
 React.render(<Home/>, mountNode);
--- app/scripts/components/home.js
+++ app/scripts/components/home.js
@@ -16,6 +16,28 @@ export default class extends React.Component {
 
   render() {
     return (
+      <div>
+        <header className="group">
+          <div className="appName">mids-sf-housing-visualization</div>
+        </header>
+
+        <div className="map-application">
+          <div id="map" className="map"></div>
+        </div>
+
+        <div className="sidebar">
+          <svg></svg>
+        </div>
+
+        <div className="time-series">
+          <svg></svg>
+        </div>
+      </div>
+    );
+  }
+
+  oldRender() {
+    return (
       <div className="hero-unit">
         <h1>'Allo, 'Allo!</h1>
         <p>This is a React component.<br/>
