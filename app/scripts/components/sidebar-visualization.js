07bb8540291997a8c6df49d587d3a506814a784f
jbocharov <john@voicebase.com>
2015-07-11 12:15:13 -0700

    Imported nv.d3.css by renamingas nv.d3.scss (HACK warning applies)

--- app/scripts/components/sidebar-visualization.js
+++ app/scripts/components/sidebar-visualization.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import d3 from 'd3';
+import nvd3 from 'd3';
 import SidebarData from '../data/sidebar-data';
 
 
@@ -22,7 +23,35 @@ export default class extends React.Component {
 
   componentDidMount() {
     console.log(document.querySelector('.sidebar'));
+    const container = '.sidebar svg';
+    let data = this.state.data;
+    nv.addGraph(function() {
+      var chart = nv.models.multiBarHorizontalChart()
+          .x(function(d) { return d.label })
+          .y(function(d) { return d.value })
+          .margin({top: 30, right: 20, bottom: 50, left: 175})
+          .showValues(true)           //Show bar value next to each bar.
+          .tooltips(true)             //Show tooltips on hover.
+          //.transitionDuration(350)
+          .showControls(true);        //Allow user to switch between "Grouped" and "Stacked" mode.
 
+      chart.yAxis
+          .tickFormat(d3.format(',.2f'));
+
+      d3.select(container)
+          .datum(data)
+          .call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    }, function() {
+      d3.selectAll(container + ' .nv-bar').on('click',
+        function(data) {
+          getDispatcher().dispatch(data);
+        }
+      );
+    });
   }
 
   oldRender() {
