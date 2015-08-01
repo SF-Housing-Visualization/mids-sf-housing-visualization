c7b9369e1c11b21187a20b7212f7dd63df9db221
jbocharov <john@voicebase.com>
2015-08-01 15:37:21 -0700

    gh-pages distribution of the app

--- app/scripts/content/introductory-page.js
+++ app/scripts/content/introductory-page.js
@@ -1,21 +1,20 @@
 import React from 'react';export default class extends React.Component {  constructor(props) { super(props); }  render() {    return (      <div>      <h2>Motivation</h2>
 <p>Two members of our group of three people have moved to the San Francisco Bay Area in the past year and forced to wrangle with the housing affordability crisis. Between sensational word of mouth and news stories, we struggled to get a grip on the reality of the crisis.</p>
-<p>For example, quotes like this suggest the housing crisis is new?</p>
+<p>For example, a quote from <a href="http://www.mercurynews.com/business/ci_25095612/high-prices-sending-bay-area-renters-and-homebuyers">this article</a> suggests the housing crisis is new?</p>
+<blockquote>
 <p>“I’ve spent my entire life on the Peninsula,” said Poncia, a 38-year-old teacher at a school in San Mateo. “I’m at a point where I have to make a major change in my career or move out due to high rents. I feel like I’m barely staying afloat as a teacher.”</p>
-<p><a href="http://www.mercurynews.com/business/ci_25095612/high-prices-sending-bay-area-renters-and-homebuyers">http://www.mercurynews.com/business/ci_25095612/high-prices-sending-bay-area-renters-and-homebuyers</a></p>
-<p>But others suggest this problem has been around for decades.</p>
+</blockquote>
+<p>But another from the same article suggests this problem has been around for decades.</p>
+<blockquote>
 <p>“I came here in the late 1990s and even then it was very clear that there had been a failure to keep up with demand going back at least 10 years before that.”</p>
-<p>This article also suggests that people are flowing to the periphery of the bay area. How much more affordable is it? Is the bay area turning into a region of elites?</p>
-<p>(<a href="http://www.bizjournals.com/sanfrancisco/print-edition/2015/01/02/the-housing-crisis-2015-forecast.html">http://www.bizjournals.com/sanfrancisco/print-edition/2015/01/02/the-housing-crisis-2015-forecast.html</a>)</p>
-<p>(<a href="http://kalw.org/post/bay-area-housing-bubble-or-housing-crisis">http://kalw.org/post/bay-area-housing-bubble-or-housing-crisis</a>)</p>
-<p>Is this a housing bubble (valuations are too high) or housing crisis (not enough housing supply)? How does the current housing problem compare to history, particularly the dot-com bubble and mid 2000’s housing bubble?</p>
-<p>Are people fleeing the bay area because housing is so unaffordable?</p>
-<p>(<a href="https://www.redfin.com/blog/2015/05/the-digital-diaspora.html#.VX9fVhNVikr">https://www.redfin.com/blog/2015/05/the-digital-diaspora.html#.VX9fVhNVikr</a>)</p>
-<p>We have our own practical questions:</p>
-<p>Where can I actually afford to live &amp; what income levels does it take to be able to afford to live in the bay area?</p>
+</blockquote>
+<p><a href="http://www.bizjournals.com/sanfrancisco/print-edition/2015/01/02/the-housing-crisis-2015-forecast.html">Another article</a> suggests that people are flowing to the periphery of the bay area. How much more affordable is it? Is the bay area turning into a region of elites?</p>
+<p><a href="http://kalw.org/post/bay-area-housing-bubble-or-housing-crisis">Yet another article</a> asks whether this is a housing bubble (valuations are too high) or housing crisis (not enough housing supply)? How does the current housing problem compare to history, particularly the dot-com bubble and mid 2000’s housing bubble?</p>
+<p><a href="https://www.redfin.com/blog/2015/05/the-digital-diaspora.html#.VX9fVhNVikr">One final article</a> asks if people are leaving Northern California altogether because housing is so unaffordable.</p>
+<p>Indeed, we have our own practical questions. Where can I actually afford to live &amp; what income levels does it take to be able to afford to live in the bay area?</p>
 <p>We found that these questions very often ached for geographical and historical context among several different variables that articles and gossip lack. This is where we hope our visualization can step in. Our goal is to provide clarity and context into a debate that often gets emotional and untethered from reality. Playing around with our visualization can both help you formulate questions about the bay area housing market and find answers.</p>
 <h2>Example</h2>
-<p>[This would be basically the same as the screen-shots we showed in midterm presentation interactive display]</p>
+<p>[This would be basically the same as the screen-shots we showed in midterm presentation interactive display or could alternatively be some of Angela’s interesting observations - still I think the difference in affordability of outlying counties vs “core” counties today vs 2005 is quite notable].</p>
 <p>We start with a broad overview of bay area affordability. We are particularly interested in San Mateo and Santa Clara counties so we highlight those. We see that those are two of the least affordable counties and that counties farther away from “the peninsula” are much more affordable. However, we notice that peak of the housing crisis there almost no affordable counties. We can focus on that period for more information and can see that in fact Santa Clara and San Mateo were relatively more affordable at the time.</p>
 <p>So, we learned a lot about some of the questions that articles and word of mouth raise. The outlying counties are much more affordable than the peninsula, so people moving makes a lot of sense. We also learned that the current crisis differs significantly from the 2005 housing bubble, in that the several counties are still quite affordable. We can also see that the affordability levels in Santa Clara and San Mateo counties are far from unprecedented, showing that this is not new a problem.</p>
       </div>    );  }}
